206a8c740555a33b6868b6e75245f5b32d457926
ndimoforaretas <ndimaret@gmail.com>
2022-06-22 08:13:54 +0200

    React ref created

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.imageRef = React.createRef();
+	}
+
 	render() {
 		const { description, urls } = this.props.image;
 
